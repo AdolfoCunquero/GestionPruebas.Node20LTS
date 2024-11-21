38bceeacbe4bceda06601ec016ee581bc4558395
Adolfo Cunquero <adolfocunquero.077@gmail.com>
2024-11-20 23:23:42 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### Para ejecutar todas las pruebas
+`npm test`
+
+### Para ejecutar un grupo de pruebas
+`npm test tests/userTest`
---
+++ tests/userTest/user.test.js
@@ -0,0 +1,11 @@
+const { sumar } = require('../../src/calculadora');
+
+describe('Pruebas de la función sumar', () => {
+  test('Debe retornar 5 cuando sumamos 2 y 3', () => {
+    expect(sumar(2, 3)).toBe(5);
+  });
+
+  test('Debe retornar 0 cuando sumamos -1 y 1', () => {
+    expect(sumar(-1, 1)).toBe(0);
+  });
+});
